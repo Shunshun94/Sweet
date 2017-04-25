b6439e80d96fc5f59a84db0c25edf1a312a596b7
Shunshun94 <shunshun9460@gmail.com>
2017-04-25 17:05:46 +0900

    common checks and login is implemented

---
+++ main/com/hiyoko/sweetplayer/CommonChecker/sweetCommonCheckOptionalValues.js
@@ -0,0 +1,76 @@
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.sweet = com.hiyoko.sweet || {};
+com.hiyoko.sweet.CommonChecker = com.hiyoko.sweet.CommonChecker || {};
+
+com.hiyoko.sweet.CommonChecker.OptionalValues = function($html, opt_conf) {
+	this.$html = $($html);
+	this.id = this.$html.attr('id');
+	
+	this.$table = this.getElementById('table');
+	this.$toggle = this.getElementById('toggle');
+	this.$summary = this.getElementById('summary');
+
+	this.table = new com.hiyoko.sweet.CommonChecker.OptionalValues.Table(
+					this.$table,
+					[{title:'', type:'check'}, {title:'名称', type:'text'}, {title:'修正値', type:'number'}]);
+	this.bindEvent();
+};
+
+com.hiyoko.util.extend(com.hiyoko.component.ApplicationBase, com.hiyoko.sweet.CommonChecker.OptionalValues);
+
+com.hiyoko.sweet.CommonChecker.OptionalValues.prototype.bindEvent = function(){
+	this.$toggle.click(function(e) {
+		this.$table.toggle();
+		this.$summary.text(this.table.getOptionalValue().text);
+	}.bind(this));
+};
+
+com.hiyoko.sweet.CommonChecker.OptionalValues.prototype.getOptionalValue = function() {
+	return this.table.getOptionalValue();
+};
+
+com.hiyoko.sweet.CommonChecker.OptionalValues.Table = function($html, cols) {
+	this.$html = $($html);
+	this.id = this.$html.attr('id');
+
+	this.renderTable(cols);
+	
+	this.bindEvent();
+};
+
+com.hiyoko.util.extend(com.hiyoko.component.TableBase, com.hiyoko.sweet.CommonChecker.OptionalValues.Table);
+
+com.hiyoko.sweet.CommonChecker.OptionalValues.Table.prototype.bindEvent = function(){
+	this.$html.change(function(e) {
+		
+	}.bind(this));
+};
+
+com.hiyoko.sweet.CommonChecker.OptionalValues.Table.prototype.getOptionalValue = function() {
+	var table = this.getTableValue();
+	var adjustedValue = 0;
+	var text = [];
+	var detail = '';
+
+	table.forEach(function(l) {
+		if(l[0]) {
+			text.push(l[1]);
+			var val = Number(l[2]);
+			adjustedValue += val;
+			if(val < 0) {
+				detail += '\n　' + l[1] + '　' + Number(l[2]);
+			} else {
+				detail += '\n　' + l[1] + '　+' + Number(l[2]);
+			}
+			
+		}
+	});
+
+	return {
+		value: (adjustedValue < 0 ? String(adjustedValue) : '+' + adjustedValue),
+		text: text.join(', '),
+		detail: detail
+	};
+};
+
---
+++ main/com/hiyoko/sweetplayer/sweetPlayer.js
@@ -0,0 +1,77 @@
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.sweet = com.hiyoko.sweet || {};
+com.hiyoko.sweet.Player = function($html) {
+	this.query = com.hiyoko.util.getQueries();
+	
+	this.$html = $html;
+	this.id = this.$html.attr('id');
+
+	this.tofServerAccess;
+	this.tofRoomAccess;
+
+	this.applications;
+	this.list;
+	
+	this.bindEvents();	
+	this.buildComponents();
+
+};
+com.hiyoko.util.extend(com.hiyoko.component.ApplicationBase, com.hiyoko.sweet.Player);
+
+com.hiyoko.sweet.Player.prototype.initRoomTitle = function() {
+	this.tofRoomAccess.getRoomInfo()
+	.done(function(r){
+		this.getElementById('title').text('【' + this.character.name + ' @ ' + r.roomName + '】');
+	}.bind(this))
+	.fail(function(r){
+		alert('Couldn\'t get DodontoF Room Info. Is URL correct?');
+	}.bind(this));	
+};
+
+com.hiyoko.sweet.Player.prototype.buildComponents = function() {
+	if(this.query.url && this.query.room && this.query.sheetId) {
+		//this.options = new com.hiyoko.sweet.Battle.OptionalValues(this.getElementById('option'));
+		com.hiyoko.VampireBlood.SW2.getSheet(this.query.sheetId).done(function(character){
+			this.character = character;
+			this.tofRoomAccess = new com.hiyoko.DodontoF.V2.Room(this.query.url, this.query.room, this.query.pass);
+			this.initRoomTitle();
+			
+			this.commonCheck = new com.hiyoko.sweet.CommonCheck(this.getElementById('commoncheck'), this.character);
+			
+		}.bind(this));
+	} else {
+		this.$html.children('div').hide();
+		new com.hiyoko.DodontoF.V2.Entrance(this.getElementById('entrance'));
+		this.getElementById('entrance').show();
+	}
+};
+
+com.hiyoko.sweet.Player.prototype.bindEvents = function(e) {
+	var self = this;
+	
+	this.$html.on('tofRoomRequest', function(e){
+		self.tofRoomAccess[e.method].apply(self.tofRoomAccess, e.args).done(e.resolve).fail(e.reject);
+	});
+	
+	this.$html.on('getStorage', function(e){
+		e.callback(localStorage.getItem(e.key) ? JSON.parse(localStorage.getItem(e.key)) : null);
+	});
+	
+	this.$html.on('setStorage', function(e){
+		localStorage.setItem(e.id, JSON.stringify(e.value));
+	});
+	
+	this.$html.on(com.hiyoko.component.InputFlow.Events.Finish, function(e) {
+		var url = './player.html?';
+		url += '&url=' + e.value.url;
+		url += '&room=' + e.value.room.no;
+		url += '&sheetId=' + e.value.option.sheetId;
+		if(e.value.room.isLocked) {
+			url += '&pass=' + e.value.password.password;
+		}
+		document.location = url;
+	});
+};
+
+
