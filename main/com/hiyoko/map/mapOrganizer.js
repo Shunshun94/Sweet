4e41f9e09c6a7d237ca2b5b725baef447e8e68c0
Shunshun94 <shunshun9460@gmail.com>
2017-05-02 00:13:34 +0900

    ref #10 first implementation

---
+++ main/com/hiyoko/map/mapOrganizer.js
@@ -0,0 +1,42 @@
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.sweet = com.hiyoko.sweet || {};
+
+com.hiyoko.sweet.MapOrganizer = function($html) {
+	this.query = com.hiyoko.util.getQueries();
+	
+	this.$html = $html;
+	this.id = this.$html.attr('id');
+	this.tofRoomAccess;
+	
+	this.bindEvents();	
+	this.buildComponents();
+};
+
+com.hiyoko.util.extend(com.hiyoko.component.ApplicationBase, com.hiyoko.sweet.MapOrganizer);
+
+com.hiyoko.sweet.MapOrganizer.prototype.buildComponents = function() {
+	if(this.query.url && this.query.room) {
+		this.tofRoomAccess = new com.hiyoko.DodontoF.V2.Room(this.query.url, this.query.room, this.query.pass);
+		this.map = new com.hiyoko.sweet.MapOrganizer.Map(this.getElementById('map'), {scale: Number(this.query.scale), size: 30});
+	} else {
+		alert('どどんとふの URL と部屋番号が入力されていません');
+		throw 'DodontoF URL and Room No. aren\'t inputted.'
+	}
+};
+
+com.hiyoko.sweet.MapOrganizer.prototype.bindEvents = function(e) {
+	var self = this;
+	
+	this.$html.on('tofRoomRequest', function(e){
+		self.tofRoomAccess[e.method].apply(self.tofRoomAccess, e.args).done(e.resolve).fail(e.reject);
+	});
+	
+	this.$html.on('getStorage', function(e){
+		e.callback(localStorage.getItem(e.key) ? JSON.parse(localStorage.getItem(e.key)) : null);
+	});
+	
+	this.$html.on('setStorage', function(e){
+		localStorage.setItem(e.id, JSON.stringify(e.value));
+	});
+};
