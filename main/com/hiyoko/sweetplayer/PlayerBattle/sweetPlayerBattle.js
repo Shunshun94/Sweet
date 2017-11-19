b4248cfddebcd258a07cbb357032c62c5dbb9a63
Shunshun94 <shunshun9460@gmail.com>
2017-11-20 00:41:45 +0900

    ref #72 start to develop

--- main/com/hiyoko/sweetplayer/PlayerBattle/sweetPlayerBattle.js
+++ main/com/hiyoko/sweetplayer/PlayerBattle/sweetPlayerBattle.js
@@ -14,7 +14,7 @@ com.hiyoko.sweet.PlayerBattle = function($html, character) {
 com.hiyoko.util.extend(com.hiyoko.component.ApplicationBase, com.hiyoko.sweet.PlayerBattle);
 
 com.hiyoko.sweet.PlayerBattle.prototype.buildComponents = function() {
-	this.options = new com.hiyoko.sweet.Battle.OptionalValues(this.getElementById('option'));
+	this.options = new com.hiyoko.sweet.PlayerBattle.OptionalValues(this.getElementById('option'));
 	this.weapons = new com.hiyoko.sweet.PlayerBattle.Weapons(this.getElementById('weapons'), this.character);
 	this.magics = new com.hiyoko.sweet.PlayerBattle.Magics(this.getElementById('magics'), this.character);
 	this.response = new com.hiyoko.sweet.PlayerBattle.Response(this.getElementById('response'), this.character);
