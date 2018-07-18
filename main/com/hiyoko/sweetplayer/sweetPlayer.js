8dafd4c2eb03caba907d486b435636eebaf08b15
Shunshun94 <shunshun9460@gmail.com>
2018-07-19 01:05:11 +0900

    とりあえず Discord できた

--- main/com/hiyoko/sweetplayer/sweetPlayer.js
+++ main/com/hiyoko/sweetplayer/sweetPlayer.js
@@ -31,24 +31,27 @@ com.hiyoko.sweet.Player.prototype.initRoomTitle = function() {
 };
 
 com.hiyoko.sweet.Player.prototype.buildComponents = function() {
-	if(this.query.url && this.query.room && this.query.sheetId) {
+	if(this.query.platform && this.query.sheetId) {
 		com.hiyoko.VampireBlood.SW2.getSheet(this.query.sheetId).done(function(character){
 			this.character = character;
 			this.saveSheetIdList(this.query.sheetId, this.character.name);
-			this.tofRoomAccess = new com.hiyoko.DodontoF.V2.Room(this.query.url, this.query.room, this.query.pass);
-			this.initRoomTitle();
-			
-			this.components = com.hiyoko.util.mergeArray(
-					com.hiyoko.sweet.Player.Children.components,
-					com.hiyoko.sweet.Player.Children.domIds, function(component, dom) {
-						var newComponent = new component(this.getElementById(dom), this.character);
-						newComponent.disable();
-						return newComponent;
-					}.bind(this));
-			this.resources = new com.hiyoko.sweet.ResourceManage(this.getElementById('resourceManage'), this.character);
-			this.talk = new com.hiyoko.sweet.Talk(this.getElementById('talk'), this.character);
-			this.list = new com.hiyoko.sweet.AppList(this.getElementById('list'), this.components);
-			this.onClickList({num: 0});
+			this.query.url = this.query.url || this.getElementById('entrance');
+			this.tofRoomAccess = io.github.shunshun94.trpg.RoomClientFactory(this.query);
+			setTimeout(()=>{
+				this.initRoomTitle();
+				this.components = com.hiyoko.util.mergeArray(
+						com.hiyoko.sweet.Player.Children.components,
+						com.hiyoko.sweet.Player.Children.domIds, function(component, dom) {
+							var newComponent = new component(this.getElementById(dom), this.character);
+							newComponent.disable();
+							return newComponent;
+						}.bind(this));
+				this.resources = new com.hiyoko.sweet.ResourceManage(this.getElementById('resourceManage'), this.character);
+				this.talk = new com.hiyoko.sweet.Talk(this.getElementById('talk'), this.character);
+				this.list = new com.hiyoko.sweet.AppList(this.getElementById('list'), this.components);
+				this.onClickList({num: 0});
+			}, 1500);
+
 		}.bind(this));
 	} else {
 		this.$html.children('div').hide();
