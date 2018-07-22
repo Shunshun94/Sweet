ab4444e968e0e02098838cb9369a69d055c010c7
Shunshun94 <shunshun9460@gmail.com>
2018-07-23 00:48:24 +0900

    issue #93 for GM

---
+++ main/com/hiyoko/sweet/sweetRoomList.js
@@ -0,0 +1,51 @@
+var com = com || {};
+com.hiyoko = com.hiyoko || {};
+com.hiyoko.sweet = com.hiyoko.sweet || {};
+com.hiyoko.sweet.RoomList = class extends com.hiyoko.component.ApplicationBase {
+	constructor($html, opt = {list:[]}) {
+		super($html, opt);
+		this.buildDom(opt.list);
+		this.buildEvent();
+	}
+
+	buildDom(list) {
+		this.$html.append(`<p>最近使った部屋</p>`);
+		list.slice(1, 10).forEach((v) => {
+			let room = $(`<div class="${this.id}-item" title="${v.url}"></div>`);
+			room.text(v.name);
+			this.$html.append(room);
+		});
+	}
+
+	buildEvent() {
+		this.getElementsByClass('item').click((e) => {
+			location.href = `${location.origin + location.pathname}${$(e.target).attr('title')}`;
+		});
+	}
+
+	toggle() {
+		this.$html.toggle(100);
+	}
+};
+
+com.hiyoko.sweet.RoomList.indexOf = (list, search = location.search) => {
+	for(var i = 0; i < list.length; i++) {
+		if(list[i].url === search) {
+			return i;
+		}
+	}
+	return -1;
+};
+com.hiyoko.sweet.RoomList.updateList = (list, name, search = location.search) => {
+	const currentIndex = com.hiyoko.sweet.RoomList.indexOf(list, search);
+	if(currentIndex !== -1) {
+		list = list.filter((v) => {
+			return v.url !== search;
+		});
+	}
+	list.unshift({
+		url: search,
+		name: name
+	});
+	return list.slice(0, 10);
+};
