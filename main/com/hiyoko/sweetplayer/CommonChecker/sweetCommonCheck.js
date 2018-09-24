d903046f9d48200954028b4033a4aa256e813275
Shunshun94 <shunshun9460@gmail.com>
2018-09-24 13:04:49 +0900

    For portions

--- main/com/hiyoko/sweetplayer/CommonChecker/sweetCommonCheck.js
+++ main/com/hiyoko/sweetplayer/CommonChecker/sweetCommonCheck.js
@@ -11,6 +11,8 @@ com.hiyoko.sweet.CommonCheck = function($html, character) {
 	
 	this.getElementById('exec').click(this.sendCommand.bind(this));
 	this.getElementById('execNoSkill').click(this.sendCommandNoSkill.bind(this));
+	this.getElementById('execDamage').click(this.sendDamageCommand.bind(this));
+	this.getElementById('execDamageNoSkill').click(this.sendDamageCommandNoSkill.bind(this));
 	this.getElementById('comment').change(this.autocompleteSkillAndStatus.bind(this))
 
 	this.getElementById('comment-clear').click(function(e) {
@@ -70,10 +72,10 @@ com.hiyoko.sweet.CommonCheck.prototype.sendCommand = function(e) {
 	this.fireEvent(event);
 };
 
-com.hiyoko.sweet.CommonCheck.prototype.sendCommandNoSkill = function(e) {
+com.hiyoko.sweet.CommonCheck.prototype.sendDamageCommand = function(e) {
 	var event = this.getAsyncEvent('tofRoomRequest').done(function(r){
 		$(e.target).notify('ダイスが振られました', {className: 'success', position: 'top'});
-		if(this.isCommentClearEvertytime) {
+		if(this.isCommentClearEvertytime()) {
 			this.getElementById('comment').val('');
 		}
 	}.bind(this)).fail(function(r){
@@ -81,8 +83,59 @@ com.hiyoko.sweet.CommonCheck.prototype.sendCommandNoSkill = function(e) {
 	});
 	
 	var option = this.option.getOptionalValue();
-	var text = com.hiyoko.util.format('2d6%s / %s (基準値：平目)%s',
+	const rate = this.getElementById('damageRate').val();
+	const crit = this.getElementById('damageCritical').val();
+	const text = `k${rate}@${crit}+${this.getElementById('skill').val()}+${this.getElementById('status').val()}${option.value} / ${this.getElementById('comment').val()} ` +
+	`(基準値：${this.getElementById('skill').children(':selected').text()}+${this.getElementById('status').children(':selected').text()}) ${option.detail}`;
+
+	const kindOfCheck = this.getElementById('comment').val().split(/[ 　]/)[0];
+	if(kindOfCheck) {
+		var saveEvent = this.getAsyncEvent('setStorageWithKey').done(function() {});
+		saveEvent.id = this.id + '-check-skill-status-relationship';
+		saveEvent.key = this.character.id + '#' + kindOfCheck;
+		saveEvent.value = [this.getElementById('skill').children(':selected').text(), this.getElementById('status').children(':selected').text()];
+		this.fireEvent(saveEvent);
+	}
+
+	event.args = [{name: this.character.name, message: text, bot:'SwordWorld2.0'}];
+	event.method = 'sendChat';
+
+	this.fireEvent(event);
+};
+
+com.hiyoko.sweet.CommonCheck.prototype.sendCommandNoSkill = function(e) {
+	var event = this.getAsyncEvent('tofRoomRequest').done(function(r){
+		$(e.target).notify('ダイスが振られました', {className: 'success', position: 'top'});
+		if(this.isCommentClearEvertytime()) {
+			this.getElementById('comment').val('');
+		}
+	}.bind(this)).fail(function(r){
+		alert('ダイスを振るのに失敗しました\n' + r.result);
+	});
+	
+	var option = this.option.getOptionalValue();
+	var text = com.hiyoko.util.format('k%s%s / %s (基準値：平目)%s',
 			option.value, this.getElementById('comment').val(), option.detail);
+	
+	event.args = [{name: this.character.name, message: text, bot:'SwordWorld2.0'}];
+	event.method = 'sendChat';
+	this.fireEvent(event);
+};
+
+com.hiyoko.sweet.CommonCheck.prototype.sendDamageCommandNoSkill = function(e) {
+	var event = this.getAsyncEvent('tofRoomRequest').done(function(r){
+		$(e.target).notify('ダイスが振られました', {className: 'success', position: 'top'});
+		if(this.isCommentClearEvertytime()) {
+			this.getElementById('comment').val('');
+		}
+	}.bind(this)).fail(function(r){
+		alert('ダイスを振るのに失敗しました\n' + r.result);
+	});
+	
+	var option = this.option.getOptionalValue();
+	const rate = this.getElementById('damageRate').val();
+	const crit = this.getElementById('damageCritical').val();
+	var text = `k${rate}@${crit}${option.value} / ${this.getElementById('comment').val()} (基準値：平目) ${option.detail}`;
 	
 	event.args = [{name: this.character.name, message: text, bot:'SwordWorld2.0'}];
 	event.method = 'sendChat';
